f173d700fefe22f8aa3e65679842cf92496cca14
Asmae Saidi El Hassani <asmaeelhassani41@gmail.com>
2026-03-16 11:01:49 +0000

    Asynchronous

---
+++ Asynchronous/Exercise1.js
@@ -0,0 +1,17 @@
+//1. Print out "Program started" at the start of your code
+  console.log("Program Started");
+//2. Create a Promise that resolves after 3 seconds 
+ const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Program complete");
+    }, 3000);
+});
+//3. Log out the promise while it's pending
+  console.log(myPromise);
+// 4. Print out "Program in progress..."
+console.log("Program in progress...");
+
+// 5. Print out "Program complete" when the promise completes
+myPromise.then((message) => {
+    console.log(message);
+});
